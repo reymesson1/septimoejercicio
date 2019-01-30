011b7145e751426df6e6bc4de2aadbecc947f742
reymesson1 <reymesson1>
2019-01-30 19:03:15 -0400

    Spacing

--- webPaa830/static/App.js
+++ webPaa830/static/App.js
@@ -48,7 +48,7 @@ var Autosuggest = Autosuggest;
 
 var moment = moment;
 
-var API_URL = 'http://localhost:8082';
+// const API_URL = 'http://localhost:8082'; 
 // const API_URL = 'http://159.203.156.208:8082'; 
 
 var API_HEADERS = {
