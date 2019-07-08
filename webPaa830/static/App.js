a1ea9e9080f132f6eb3db972f09aa63365e28297
messore <rey.messon@verizon.com>
2019-07-08 15:28:41 -0400

    Update App.js

--- webPaa830/static/App.js
+++ webPaa830/static/App.js
@@ -2652,7 +2652,7 @@ var MasterModal = function (_React$Component16) {
             var MasterModalEN = React.createElement(
                 Modal.Title,
                 null,
-                'Modal Header '
+                'Modal Headers'
             );
 
             var MasterModalES = React.createElement(
