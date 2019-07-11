c5f7fcd988ed1d40a0e3370752184f075260915a
messore <rey.messon@verizon.com>
2019-07-11 07:25:13 -0400

    Working on Ordering saving customer name
    In this branch we are able to see those change available in the production.
    
    Description:
    When the order are creating the items aren't taking their customer name. It's means that the user should payattention to the items that are saving.

--- webPaa830/static/App.js
+++ webPaa830/static/App.js
@@ -50,8 +50,8 @@ var moment = moment;
 
 var global = 0;
 
-// const API_URL = 'http://localhost:8082';  
-var API_URL = 'http://159.203.156.208:8082';
+var API_URL = 'http://localhost:8082';
+// const API_URL = 'http://159.203.156.208:8082';
 
 var API_HEADERS = {
 
